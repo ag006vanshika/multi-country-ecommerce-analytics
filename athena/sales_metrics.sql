48c5bc68a2764adf52b20bba532fd066bd7191f0
ag006vanshika <ag006vanshika@gmail.com>
2026-06-05 02:04:18 +0530

    Update README.md

--- athena/sales_metrics.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE VIEW sales_metrics AS
-SELECT
-    country,
-    category,
-    SUM(sales) AS revenue,
-    SUM(profit) AS profit,
-    COUNT(order_id) AS orders
-FROM orders_processed
-GROUP BY country, category;
